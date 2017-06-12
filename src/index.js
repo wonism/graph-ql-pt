43b4f1f46b50af9780a0c9790c6742e01b23afbc
wonism <jaewonism57@gmail.com>
2017-06-13 00:37:38 +0900

    graphql pt

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
+import $ from 'jquery';
 import '../scss/index.scss';
 
+$(document).ready(() => {
+  $('body')
+    .on('click', '#go', () => {
+      $('.introduce').addClass('none');
+      $('.example').removeClass('none');
+    })
+    .on('click', '#submit-1, #submit-2', (e) => {
+      const target = e.target;
+      const id = target.id.replace(/\D/g, '');
+      const formData = new FormData();
+      const query = id === '1' ? `
+      {
+        user(id:"1") {
+          email
+        }
+      }` : `
+      {
+        user(id: "1") {
+          email,
+          age
+        }
+      }`;
+
+      formData.append('query', query);
+
+      fetch('/graphql', {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error(res.statusText);
+          }
+        })
+        .then((res) => { console.log(res.data); })
+        .catch(() => {} );
+    });
+});
